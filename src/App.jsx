335c7d8f81f28fb5ff83c957ef7d311ba9f33c57
ackwolver335 <abhaych335@gmail.com>
2025-08-04 19:49:25 +0530

    Project Basic Setup Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+// required CSS files regarding some changes
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// components regarding navbar and footer 
+import Navbar from './components/Navbar'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// pages regarding different sections
+import Home from './pages/Home'
+
+// packages regarding navigation and page routing
+import { Routes,Route } from 'react-router-dom'
+
+function App() {
+
+    return (
+        <>
+           
+            {/* Common Navbar regarding page's redirection */}
+            <Navbar />
+
+            {/* Routes regarding different pages navigation */}
+            <Routes>
+                <Route index path="/" element={<Home />} />
+            </Routes>
+
+        </>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// routes regarding future flags
+const router = createBrowserRouter(
+  [
+    {
+      path : "/*",
+      element : <App />
+    },
+  ],
+  {
+    future : {
+      v7_startTransition : true,
+    },
+  }
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </StrictMode>,
 )
